7ae6587114b0ee396e5866c125ddb94fe1f372b6
root <root@ip-172-31-35-204.ap-south-1.compute.internal>
2026-03-03 16:23:10 +0000

    this is the chanage from the linux and missed out pull

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ and another line for the changes to be done for the practice.
 
 this is the change done from the linux developer
 
+
+I missed out the bash line here did not pull and updating the forth line let me see what happens
